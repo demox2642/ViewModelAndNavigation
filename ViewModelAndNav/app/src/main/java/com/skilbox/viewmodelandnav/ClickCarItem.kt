467813b6128266c1fb776cc87f29045daa5a11d4
Anton Gorodetskiy <demox2642@gmail.com>
2021-08-16 10:36:46 +0500

    View Model and Navigation

---
+++ ViewModelAndNav/app/src/main/java/com/skilbox/viewmodelandnav/ClickCarItem.kt
@@ -0,0 +1,13 @@
+package com.skilbox.viewmodelandnav
+
+class ClickCarItem (_itLongClick:Boolean,
+                                             _id:Long,
+                                             _title:String
+  ) {
+
+    var itLongClick:Boolean = _itLongClick
+    var  id:Long = _id
+    var  title:String = _title
+
+
+}
---
+++ ViewModelAndNav/app/src/main/java/com/skilbox/viewmodelandnav/GLM/DetailFragmentGLM.kt
@@ -0,0 +1,40 @@
+package com.skilbox.viewmodelandnav.GLM
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.navGraphViewModels
+import com.bumptech.glide.Glide
+import com.skilbox.viewmodelandnav.R
+import kotlinx.android.synthetic.main.car_class_a.view.*
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+class DetailFragmentGLM : Fragment(R.layout.fragment_detail) {
+    private val args:DetailFragmentGLMArgs by navArgs()
+
+
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        car_text_view.text = "Element ID = "+args.carId.toString()
+        detail_car_brend.text = "Car Brend = " + args.carBrend
+        detail_car_name.text = "Car Name = " + args.carName
+        detail_car_engine_capacity.text = "Engine capacity = " + args.carEngineCapacity
+
+        Glide.with(requireContext())
+            .load(args.carImage)
+            .placeholder(R.drawable.load_imege)
+            .into(detail_car_image)
+            .view
+
+
+    }
+
+}
